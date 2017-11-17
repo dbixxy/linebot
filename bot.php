fd1f24229ea98a374922d434dcccbae7fc6a1294
dbixxy <dbixxy@gmail.com>
2017-11-17 15:04:03 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -1,14 +1,6 @@
 <?php
-
-// example: https://github.com/onlinetuts/line-bot-api/blob/master/php/example/chapter-01.php
-
-include ('line-bot-api/php/line-bot.php');
-
-$channelSecret = '6963da5f9026af3311962c9ff23e4b81';
 $access_token = '7Af83hBtZ+AAdf+fxdaO09IqhGUpgqoW9LVk1Zkp3FcYFRGvICOikhUk8Qc63bmRtbuRCUbHL9qp9AxtfOVlvUnBJOxWuJ6EgqlZSxCCALCBdpHNuzJ7VjcJUVnfL3XSIB9cVV+s8wcOUOlghs6raAdB04t89/1O/w1cDnyilFU=';
 
-$bot = new BOT_API($channelSecret, $access_token);
-	
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
